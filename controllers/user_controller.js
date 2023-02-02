226f35658b9ad30137584144480b48c9e56c8e6d
elReclusa <howell_j@hotmail.com>
2023-02-01 20:07:56 -0600

    Added models and date format

---
+++ controllers/user_controller.js
@@ -0,0 +1,96 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) = {
+        User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+},
+
+getUserById({  _id: params.id })
+.populate(
+    {
+        path: 'thoughts',
+        select: '-__v'
+    })
+    .populate(
+        {
+            path: 'friends',
+            select: '-__v'
+        })
+        .then(dbUserData => {
+            if (!dbUserData){
+
+            res.status(404).json({ message: "Invalid User Id."})
+            return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }, 
+
+    createSecureServer({ body}, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err = res.status(400).json(err));
+    },
+
+    updateUser({ params, body}, res) {
+        User.findOneAndUpdate({ _id:params.id}, body. { new: true})
+        .then(dbUserData => {
+if (!dbUserData) {
+    res.status(404).json({ message: "Invalid User Id." });
+    return;
+    }
+    res,json(dbUserData)
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "Invalid User Id." });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    addFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId },
+            { $push: { friends: params.friendId } },
+            { new: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "Invalid User Id."});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "Invalid User Id." });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+
+    module.exports = userController;
